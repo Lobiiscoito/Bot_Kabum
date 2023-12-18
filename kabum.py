84788d3c9547e989db9d8ce1e249209899d0aea2
Gustavo <81110350+Lobiiscoito@users.noreply.github.com>
2023-12-18 17:06:18 -0300

    Add files via upload

---
+++ kabum.py
@@ -0,0 +1,111 @@
+import requests
+import parsel
+import psycopg2
+from datetime import datetime
+from telegram import Bot
+from time import sleep
+from links import links_urls
+import asyncio
+
+banco_de_dados = "amazon_bot"
+usuario_bd = "postgres"
+senha_bd = "invasao22"
+bot_token = "6328944662:AAEP3cQfgdmFW6Bf8VsMy8_9sl7Qj9MalEk"
+chat_id = "@precospromo"
+
+async def enviar_mensagem(nome_produto, valor_anterior, preco_float, link):
+    bot = Bot(token=bot_token)
+    await bot.send_message(chat_id=chat_id,text=f"Entrou em Promoção \n\nO {nome_produto} \n💵 De R$ {valor_anterior} para R$ {preco_float}\n\n🔗{link}.")
+
+async def rastreamento_precos(urls, banco_de_dados, usuario_bd, senha_bd, bot_token, chat_id):
+    valores_atuais = {}
+    valores_antigos = {}
+
+    while True:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0',
+        }
+
+        for url in urls:
+            req = requests.get(url, headers=headers)
+            if req.status_code == 200:
+                sel = parsel.Selector(text=req.text)
+                nome_produto = sel.xpath('//h1[@class]/text()').get().strip()[:40]
+                preco_produto = sel.xpath('//h4[@class]//text()').get()
+
+                if preco_produto and any(char.isdigit() for char in preco_produto):
+                    preco_float = float(preco_produto.replace('.', '').replace(',', '.').replace('R$', '').strip())
+                    link = url
+                    data_hora = lambda: datetime.now().strftime("%d/%m/%y %H:%M")
+
+                    if url in valores_atuais:
+                        valor_anterior = valores_atuais[url]
+                        valores_antigos[url] = valor_anterior
+
+                        if preco_float < valor_anterior:
+                            conn = psycopg2.connect(database=banco_de_dados, user=usuario_bd, password=senha_bd,host="localhost", port="5433")
+                            cur = conn.cursor()
+                            cur.execute(
+                                "UPDATE produtos SET preco_desconto = %s, data_hora = %s WHERE nome_produto = %s",
+                                (preco_float, data_hora, nome_produto))
+                            conn.commit()
+                            conn.close()
+                            preco_atual_formatado = '{:.2f}'.format(preco_float).replace('.', ',')
+                            preco_anterior_formatado = '{:.2f}'.format(valor_anterior).replace('.', ',')
+                            await enviar_mensagem(nome_produto, preco_anterior_formatado, preco_atual_formatado, link)
+                            print("mensagem enviada")
+                        elif preco_float == valor_anterior:
+                            print(f"O produto {nome_produto} continua com o mesmo preço {preco_produto}.")
+
+                    if url not in valores_atuais:
+                        if not produto_existe_no_banco(nome_produto, link):
+                            print(f"O produto {nome_produto} {preco_produto} está sendo adicionado pela primeira vez.")
+                            conn = psycopg2.connect(database=banco_de_dados, user=usuario_bd, password=senha_bd,
+                                                    host="localhost", port="5433")
+                            cur = conn.cursor()
+                            cur.execute(
+                                "INSERT INTO produtos (nome_produto,preco_atual,link_produto,data_hora) VALUES (%s,%s,%s,%s)",
+                                (nome_produto, preco_float, link, data_hora()))
+                            conn.commit()
+                            print("Os valores foram adicionados ao banco de dados")
+                            conn.close()
+                        else:
+                            print(
+                                f"O produto {nome_produto} já existe no banco de dados. Não será adicionado novamente.")
+
+                    valores_atuais[url] = preco_float
+                    valores_atuais[url] = preco_float
+
+                else:
+                    print(f'O produto {nome_produto} não possui um preço válido: {preco_produto}')
+            else:
+                print('Não foi possível encontrar os dados para a URL: ' + url)
+
+        sleep(60)
+
+def produto_existe_no_banco(nome_produto, link):
+    conn = psycopg2.connect(database=banco_de_dados, user=usuario_bd, password=senha_bd, host="localhost", port="5433")
+    cur = conn.cursor()
+    cur.execute("SELECT COUNT(*) FROM produtos WHERE nome_produto = %s OR link_produto = %s", (nome_produto, link))
+    count = cur.fetchone()[0]
+    conn.close()
+    return count > 0
+
+if __name__ == "__main__":
+    urls_nvme = links_urls('nvme')
+    urls_fonte = links_urls('fonte')
+    urls_placa_de_video = links_urls('placa_de_video')
+    urls_monitor = links_urls('monitor')
+    urls_headset = links_urls('headset')
+    for url in urls_nvme:
+        pass
+    for url in urls_fonte:
+        pass
+    for url in urls_placa_de_video:
+        pass
+    for url in urls_monitor:
+        pass
+    for url in urls_headset:
+        pass
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(rastreamento_precos(urls_nvme + urls_fonte + urls_placa_de_video + urls_monitor + urls_headset , banco_de_dados, usuario_bd, senha_bd, bot_token, chat_id))
---
+++ links.py
@@ -0,0 +1,31 @@
+
+def links_urls(categoria):
+    if categoria == 'nvme':
+        return [
+            'https://www.kabum.com.br/produto/380745/ssd-1-tb-kingston-nv2-m-2-2280-pcie-nvme-leitura-3500-mb-s-e-gravacao-2100-mb-s-snv2s-1000g?gad_source=4'
+            ]
+    elif categoria == 'fonte':
+        return ['https://www.kabum.com.br/produto/369658/fonte-msi-mag-a650bn-atx-650w-80-plus-bronze-pfc-ativo-entrada-bivolt-preto-306-7zp2b22-ce0',
+                'https://www.kabum.com.br/produto/132783/fonte-corsair-cv650-650w-80-plus-bronze-cp-9020236-br',
+                'https://www.kabum.com.br/produto/114269/fonte-xpg-pylon-650w-80-plus-bronze-pylon650b-bkcbr'
+
+        ]
+    elif categoria == 'placa_de_video':
+        return ['https://www.kabum.com.br/produto/470987/placa-de-video-rtx-4060-eagle-oc-gigabyte-nvidia-geforce-8gb-gddr6-dlss-ray-tracing-gv-n4060eagle-oc-8gd',
+                'https://www.kabum.com.br/produto/455712/placa-de-video-nvidia-zotac-geforce-rtx-3060-ti-twin-edge-8gb-ddr6-256-bits-zt-a30620e-10p',
+
+
+        ]
+    elif categoria == 'monitor':
+        return ['https://www.kabum.com.br/produto/444038/monitor-gamer-lg-ultragear-27-full-hd-144hz-1ms-ips-hdmi-e-displayport-hdr-10-99-srgb-freesync-premium-vesa-27gn65r',
+                'https://www.kabum.com.br/produto/434391/monitor-gamer-lg-ultragear-24-full-hd-144hz-1ms-ips-hdmi-e-displayport-99-srgb-hdr-freesync-premium-vesa-24gn60r-b-awzm',
+                'https://www.kabum.com.br/produto/364670/monitor-gamer-samsung-odyssey-g32-27-led-full-hd-165-hz-1ms-hdmi-displayport-freesync-premium-ajuste-de-altura-preto-ls27ag320nlxzd'
+                ]
+
+    elif categoria == 'headset':
+       return ['https://www.kabum.com.br/produto/386864/headset-gamer-sem-fio-hyperx-cloud-alpha-som-surround-7-1-drivers-50mm-usb-300-horas-de-bateria-preto-e-vermelho-4p5d4aa',
+         'https://www.kabum.com.br/produto/467535/headset-gamer-hyperx-cloud-iii-dts-drivers-53mm-usb-pc-ps5-ps4-xbox-series-xis-xbox-one-nintendo-switch-mobile-preto-e-vermelho-727a9aa,'
+         ]
+
+
+
